7d0e7d52b9818313c8cf74cc089e8d5b115f222a
Sahil Phondekar <sahil.jphondekar@gmail.com>
2022-08-05 21:33:13 +0530

    Firestore stream and listing brew data

--- flutter_firebase/lib/screens/home/home.dart
+++ flutter_firebase/lib/screens/home/home.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/models/brew.dart';
+import 'package:flutter_firebase/screens/home/brew_list.dart';
 import 'package:flutter_firebase/services/auth.dart';
+import 'package:flutter_firebase/services/database.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
@@ -7,21 +12,25 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: const Text('Brew Crew'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: [
-          TextButton.icon(
-              onPressed: () async {
-                _auth.signOut();
-              },
-              icon: const Icon(Icons.person),
-              label: const Text("Logout"))
-        ],
-      ),
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      initialData: const [],
+      child: Scaffold(
+          backgroundColor: Colors.brown[50],
+          appBar: AppBar(
+            title: const Text('Brew Crew'),
+            backgroundColor: Colors.brown[400],
+            elevation: 0.0,
+            actions: [
+              TextButton.icon(
+                  onPressed: () async {
+                    _auth.signOut();
+                  },
+                  icon: const Icon(Icons.person),
+                  label: const Text("Logout"))
+            ],
+          ),
+          body: const BrewList()),
     );
   }
 }
--- flutter_firebase/lib/services/database.dart
+++ flutter_firebase/lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_firebase/models/brew.dart';
 
 class DatabaseService {
   final String uid;
@@ -6,11 +7,25 @@ class DatabaseService {
   final CollectionReference brewCollection =
       FirebaseFirestore.instance.collection('brews');
 
-  DatabaseService({required this.uid});
+  DatabaseService({this.uid = ''});
 
   Future<void> updateUserData(String sugars, String name, int strength) async {
     return await brewCollection
         .doc(uid)
         .set({'sugars': sugars, 'name': name, 'strength': strength});
+  }
+
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return Brew(
+          name: doc.get('name'),
+          strength: doc.get('strength'),
+          sugars: doc.get('sugars'));
+    }).toList();
+  }
+
+  // get brew stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
