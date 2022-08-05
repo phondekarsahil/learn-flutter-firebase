0d8e5733b44c7c9149e64c10144fc0ad8cf953e3
Sahil Phondekar <sahil.jphondekar@gmail.com>
2022-08-05 21:03:57 +0530

    Firestore database setup

--- flutter_firebase/lib/services/auth.dart
+++ flutter_firebase/lib/services/auth.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_firebase/models/custom_user.dart';
+import 'package:flutter_firebase/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -33,7 +34,6 @@ class AuthService {
       User? user = result.user;
       return _userFromFirebaseUser(user);
     } catch (e) {
-      print(e.toString());
       return null;
     }
   }
@@ -44,9 +44,11 @@ class AuthService {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User? user = result.user;
+      // create a new document for the user with the uid
+      await DatabaseService(uid: user!.uid)
+          .updateUserData('0', 'new crew member', 100);
       return _userFromFirebaseUser(user);
     } catch (e) {
-      print(e.toString());
       return null;
     }
   }
@@ -56,7 +58,6 @@ class AuthService {
     try {
       return await _auth.signOut();
     } catch (e) {
-      print(e.toString());
       return null;
     }
   }
---
+++ flutter_firebase/lib/services/database.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final String uid;
+  // collection reference
+  final CollectionReference brewCollection =
+      FirebaseFirestore.instance.collection('brews');
+
+  DatabaseService({required this.uid});
+
+  Future<void> updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection
+        .doc(uid)
+        .set({'sugars': sugars, 'name': name, 'strength': strength});
+  }
+}
